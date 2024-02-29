85eb5017a00046dd4d86620e122563a537620bf4
driver001 <andres.udo.info@gmail.com>
2024-02-29 12:28:45 -0400

    cambios en diseño de vista de home

--- src/components/pages/public/home/Home.tsx
+++ src/components/pages/public/home/Home.tsx
@@ -1,48 +1,124 @@
-import { /* Button, */ Col, Row, Typography } from "antd";
+import {
+  /* Button, */ Avatar,
+  Button,
+  Col,
+  Row,
+  Typography,
+  theme,
+} from "antd";
 import { useTranslation } from "react-i18next";
 // import { Link } from "react-router-dom";
 import ImageYCC from "../../../ui-components/ImageYCC";
+import { Link } from "react-router-dom";
+import {
+  ownerFacebookUrl,
+  ownerTwitterUrl,
+  ownerInstagramUrl,
+} from "../../../../app.config.json";
+import {
+  CopyrightOutlined,
+  FacebookFilled,
+  HeartFilled,
+  InstagramFilled,
+  TwitterCircleFilled,
+} from "@ant-design/icons";
+
+const { useToken } = theme;
 
 const { Paragraph, Title } = Typography;
 
 export const Home = () => {
   const { t } = useTranslation();
+  const { token } = useToken();
 
   return (
     <Row
       align={"middle"}
       justify={"center"}
-      style={{ height: "100%", width: "90%", margin: "0 auto", padding: 24 }}
+      style={{
+        height: "100%",
+        width: "100%",
+        margin: "0 auto",
+        padding: "24px 5%",
+        background: "#f1f1f1",
+      }}
     >
       <Col span={12}>
         <Title
           style={{
             marginBottom: 0,
-            marginTop: "2%",
+
             textAlign: "center",
             userSelect: "none",
-            fontSize: 42,
+            fontSize: 48,
           }}
           level={3}
         >
-          {t("Welcome to Yourcareconnects")}
+          {t("Welcome to ")}
+          <span style={{ color: "rgb(238 191 57)" }}>Yourcareconnects</span>
         </Title>
         <Paragraph
           style={{
             marginBottom: 0,
             textAlign: "center",
             userSelect: "none",
-            fontSize: 26,
+            fontSize: 30,
           }}
         >
           {t("Connecting Customers and Providers")}
         </Paragraph>
-
-        <Row
-          style={{ justifyContent: "center", marginTop: "7%", width: "100%" }}
-        >
+        <Col flex="1 0 auto" style={{ textAlign: "center", marginTop: 12 }}>
+          <Link
+            style={{ marginRight: 12 }}
+            className="--footer__avatar"
+            to={ownerFacebookUrl}
+            target="_blank"
+          >
+            <Avatar
+              size={"large"}
+              style={{
+                fontSize: 20,
+                backgroundColor: token.colorPrimary,
+              }}
+            >
+              <FacebookFilled style={{ color: "#1a1a13" }} />
+            </Avatar>
+          </Link>
+          <Link
+            style={{ marginRight: 12 }}
+            className="--footer__avatar"
+            to={ownerTwitterUrl}
+            target="_blank"
+          >
+            <Avatar
+              size={"large"}
+              style={{
+                fontSize: 20,
+                backgroundColor: token.colorPrimary,
+              }}
+            >
+              <TwitterCircleFilled style={{ color: "#1a1a13" }} />
+            </Avatar>
+          </Link>
+          <Link
+            className="--footer__avatar"
+            to={ownerInstagramUrl}
+            target="_blank"
+          >
+            <Avatar
+              size={"large"}
+              style={{
+                fontSize: 20,
+                backgroundColor: token.colorPrimary,
+              }}
+            >
+              <InstagramFilled style={{ color: "#1a1a13" }} />
+            </Avatar>
+          </Link>
+        </Col>
+        <Row style={{ justifyContent: "left", marginTop: 12, width: "100%" }}>
           {/*  <Link to="/login">
-            <Button type="primary" style={{ width: 150 }}>
+            <Button size="large" type="primary" style={{ width: 150 }}>
               {t("Login")}
             </Button>
           </Link> */}
